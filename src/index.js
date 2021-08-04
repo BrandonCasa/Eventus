2a2f5d2ac3b0372dd407d7ca2b4559f626c80042
Brandon Casamichana <brandoncasawork8@gmail.com>
2021-08-04 10:13:47 -0400

    Changed stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuthProvider, FirebaseAuthConsumer, IfFirebaseAuthed, IfFirebaseAuthedAnd } from "@react-firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRYFrKMQ8RDMZF51yrS2rfJAuxdwjJ3A4",
+  authDomain: "eventus-2a71a.firebaseapp.com",
+  projectId: "eventus-2a71a",
+  storageBucket: "eventus-2a71a.appspot.com",
+  messagingSenderId: "1028110222972",
+  appId: "1:1028110222972:web:6477be3830e5751755ebf9",
+  measurementId: "G-GXJ3RD62BY",
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </FirebaseAuthProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
